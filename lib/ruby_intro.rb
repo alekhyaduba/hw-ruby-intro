424b624ca32c0a6dbed604a54aa4c21b29962372
Alekhya Duba <alekhya.ramarao@gmail.com>
2022-02-05 22:27:53 +0000

    add sum_t0_n? implementation

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -35,7 +35,15 @@ def max_2_sum arr
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  for i in 0..arr.length()-2
+    for j in i+1..arr.length()-1
+      if arr[i] + arr[j] == n
+        return true
+        
+      end
+    end
+  end
+  return false
 end
 
 # Part 2
